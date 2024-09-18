c8151e4f332c49945b031bd6a3863cbef1f371d2
Dominic <Domdombossy@gmail.com>
2024-09-18 15:57:49 -0400

    Updated Code, now it actually outputs the value

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        double itemCost = 28;
+        final double TAX_RATE = 0.05;
+        double taxCost = (itemCost * TAX_RATE) + itemCost;
+        System.out.println("The cost of your item was: " + itemCost + " and the cost of your item with a 5% tax is: " + taxCost);
+
 
 
     }
